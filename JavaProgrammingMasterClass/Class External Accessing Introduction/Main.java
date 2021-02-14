deea1769e13ce0602f99e1ce6a59f518eb51e7c6
manan-crypto <75381077+mananaggarwal2001@users.noreply.github.com>
2021-02-14 23:25:29 +0530

    Add files via upload

---
+++ JavaProgrammingMasterClass/Class External Accessing Introduction/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+       BankAccount formalities=new BankAccount();
+       formalities.setAccountnumber(1212121212);
+        System.out.println(formalities.getAccountnumber());
+    }
+}
