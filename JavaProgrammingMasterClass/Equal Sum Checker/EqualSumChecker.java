3c4cbada0e0fb5be4d0d946bc6682f5217652d02
AnonyomousManan <75381077+mananaggarwal2001@users.noreply.github.com>
2021-02-15 22:43:16 +0530

    Add files via upload

---
+++ JavaProgrammingMasterClass/Equal Sum Checker/EqualSumChecker.java
@@ -0,0 +1,13 @@
+public class EqualSumChecker {
+    public static boolean hasEqualSum(int num1,int num2,int num3) {
+        {
+            if(num3==(num1+num2))
+            {
+                return true;
+            }else{
+                return false;
+            }
+        }
+
+    }
+}
