d19e2ea8bd41626596a498af1d9b7b7f5f1146c0
AnonyomousManan <75381077+mananaggarwal2001@users.noreply.github.com>
2021-02-15 23:57:37 +0530

    Add files via upload

---
+++ JavaProgrammingMasterClass/Point Class/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Point point=new Point();
+        point.setX(23);
+        point.setY(46);
+        System.out.println(point.distance(32,33));
+    }
+}
