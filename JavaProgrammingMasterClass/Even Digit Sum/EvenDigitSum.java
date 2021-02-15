4a0a8a9bc39f248096d97a14ccb0cc2be226ce59
AnonyomousManan <75381077+mananaggarwal2001@users.noreply.github.com>
2021-02-15 22:45:57 +0530

    Add files via upload

---
+++ JavaProgrammingMasterClass/Even Digit Sum/EvenDigitSum.java
@@ -0,0 +1,18 @@
+public class EvenDigitSum {
+    public static int getEvenDigitSum(int number){
+        if(number<0){
+            return -1;
+        }
+        int sum=0;
+        int digit;
+        while(number>0){
+            digit=number%10;
+            number=number/10;
+            if(digit%2==0)
+            {
+                sum+=digit;
+            }
+        }
+        return sum;
+    }
+}
