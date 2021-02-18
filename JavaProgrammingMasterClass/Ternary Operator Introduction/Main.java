f4d90ffd837aaee1da42691a34aebafb4072da56
AnonyomousManan <75381077+mananaggarwal2001@users.noreply.github.com>
2021-02-18 14:59:41 +0530

    Add files via upload

---
+++ JavaProgrammingMasterClass/Ternary Operator Introduction/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	boolean iscar = false;
+    boolean wascar=    iscar? true: false;
+        System.out.println("Car = "+ wascar);
+
+
+    }
+}
