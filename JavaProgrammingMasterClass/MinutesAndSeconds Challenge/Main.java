1469cf1c9056858422d715780f7d74b32102ff3d
AnonyomousManan <75381077+mananaggarwal2001@users.noreply.github.com>
2021-02-18 14:36:20 +0530

    Add files via upload

---
+++ JavaProgrammingMasterClass/MinutesAndSeconds Challenge/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String hour=SecondAndMinuteChallenge.getGetDurationString(700,56);
+        System.out.println(hour);
+        System.out.println(SecondAndMinuteChallenge.getGetDurationString(567899));
+
+    }
+}
