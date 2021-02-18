7017fcf5955b63e19da8faff33f9ec61659a48bd
AnonyomousManan <75381077+mananaggarwal2001@users.noreply.github.com>
2021-02-18 15:08:26 +0530

    Add files via upload

---
+++ JavaProgrammingMasterClass/Vip Customer Class/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        VipCustomer customer=new VipCustomer();
+        System.out.println("Name is :" + customer.getName());
+        System.out.println(" Credit Limit is " + customer.getCreditlimit());
+        System.out.println("Email Addresss is " + customer.getEmail_address());
+    }
+}
