4ac08dbdeddf98acb6fb757210ab388bcbf55f0f
AnonyomousManan <75381077+mananaggarwal2001@users.noreply.github.com>
2021-07-04 20:25:03 +0530

    Delete Python/Practice_Questions directory

--- Python/Practice_Questions/Dictionary.py
+++
@@ -1,5 +0,0 @@
-import os
-# we import the os directory from the inbuild module
-print(os.listdir('D:\\Manan Work\\Programming in the Python\\Python Notes'))
-# we use the os.listdir() to list the directory of the contents
-# \\ is the escape sequence characters
--- Python/Practice_Questions/playSound.py
+++
@@ -1,2 +0,0 @@
-from playsound import playsound
-playsound('D:\\Manan Work\\Programming in the Python\\Python Programs\Practice_Questions\\retro.wav')
