2510cc18c555fbe5887abd5be9612407d61bd28a
AnonyomousManan <75381077+mananaggarwal2001@users.noreply.github.com>
2021-07-04 18:03:33 +0530

    Add files via upload

---
+++ Python/01.hello.py
@@ -0,0 +1,11 @@
+# Author = Manan Aggarwal
+# license= Gta 5
+# for commenting the single line
+# for multiple line comment we use the '''
+# for example
+'''Manan is a good boy
+Manan is the best in the world
+'''
+import os
+# importing the os module
+print("Hello World")
---
+++ Python/Practice_Questions/playSound.py
@@ -0,0 +1,2 @@
+from playsound import playsound
+playsound('D:\\Manan Work\\Programming in the Python\\Python Programs\Practice_Questions\\retro.wav')
